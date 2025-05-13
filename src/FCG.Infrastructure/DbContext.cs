fd63578044bba8175e461c0d3c047eece95a5b5b
Daniel <daniel.pontes@xavipay.com>
2025-05-13 12:41:23 -0300

    Add project files.

---
+++ src/FCG.Infrastructure/DbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FCG.Infrastructure
+{
+    public class AppDbContext :  IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        
+    }
+}
