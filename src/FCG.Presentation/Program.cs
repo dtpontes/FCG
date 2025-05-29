cab1d7688e36b9ae091111a5b8e6a83a119a9c51
Daniel <daniel.pontes@xavipay.com>
2025-05-29 13:19:49 -0300

    Implementação do Container

--- src/FCG.Presentation/Program.cs
+++ src/FCG.Presentation/Program.cs
@@ -26,6 +26,12 @@ builder.Host.ConfigureSerilog();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
