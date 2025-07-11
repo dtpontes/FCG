8379affe87bcd32a0c402567a5ed790f92b0f2a1
Daniel Teixeira Pontes <dtpontes@hotmail.com>
2025-07-11 15:35:33 -0300

    teste

--- src/FCG.Presentation/Controllers/ClientController.cs
+++ src/FCG.Presentation/Controllers/ClientController.cs
@@ -68,20 +68,6 @@ namespace FCG.Presentation.Controllers
             return Response(true);
         }
 
-        /// <summary>
-        /// Teste de atualização
-        /// </summary>        
-        /// <returns>Cliente criado ou erros de validação</returns>
-        [HttpGet("teste3")]
-        [ProducesResponseType(typeof(RegisterClientResponseDto), 200)]
-        [ProducesResponseType(400)]
-        public async Task<IActionResult> Test3()
-        {
-            // Cria o cliente associado ao usuário            
-
-            return Response(true);
-        }
-
 
     }
 }
